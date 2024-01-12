bf086211abcaf152439c58342c500b028270ea68
jvx0 <154292278+jvx0@users.noreply.github.com>
2024-01-11 18:55:32 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,15 +52,15 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("silence.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("background_soundmp3.mp3");
+export var ambientSound = new soundFile("ambience.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -25; //turn down volume
+ambientSound.player.volume.value = -30; //turn down volume
 
-export var adventureMusic = new soundFile("silence.mp3");
+export var adventureMusic = new soundFile("background_soundmp3.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
-adventureMusic.player.volume.value = -10;
+adventureMusic.player.volume.value = -5;
 
 export var villageMusic = new soundFile("silence.mp3");
 soundArray.push(villageMusic);
